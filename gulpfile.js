f93b643407364c22a0ed8c95e2a4ccdf26ac55e7
Tim Lucas <t@toolmantim.com>
2015-06-17 21:38:53 +1000

    Sourcemaps!

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function () {
   gulp.src('./theme/sass/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./theme'));
 });
 
