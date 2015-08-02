911fa4ca44edd26924c68fdac4f47df47f9d1a8a
Josh Bassett <josh.bassett@gmail.com>
2015-08-02 20:05:03 +1000

    Configure labels for event post type.

--- theme/inc/event-post-type.php
+++ theme/inc/event-post-type.php
@@ -5,16 +5,28 @@
  * @package Victorian Climbing Club
  */
 
-function register_events_post_type() {
+function register_event_post_type() {
+  $labels = array(
+    'name'          => 'Events',
+    'singular_name' => 'Event',
+    'add_new_item'  => 'Add New Event'
+  );
   $args = array(
-    'public' => true,
-    'has_archive' => true,
-    'label' => 'Events',
-    'rewrite' => array('slug' => 'events'),
-    'supports' => array('title', 'editor')
+    'public'            => true,
+    'has_archive'       => true,
+    'show_in_nav_menus' => true,
+    'labels'            => $labels,
+    'rewrite'           => array('slug' => 'events'),
+    'supports'          => array('title', 'editor')
   );
   register_post_type('event', $args);
 }
 
-add_action('init', 'register_events_post_type');
-add_action('after_switch_theme', 'flush_rewrite_rules');
+add_action('init', 'register_event_post_type');
+
+function my_rewrite_flush() {
+  register_event_post_type();
+  flush_rewrite_rules();
+}
+
+add_action('after_switch_theme', 'my_rewrite_flush');
