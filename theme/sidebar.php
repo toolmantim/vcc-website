c4e2d37307bcc15f5c4640cb3339290c21d360e1
Tim Lucas <t@toolmantim.com>
2015-11-01 17:03:46 +1100

    Link to CliffCare page

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -11,7 +11,7 @@ if ( is_active_sidebar( 'homepage-cliffcare-sidebar' ) ) :
 <div class="home__cliffcare-sidebar" role="complementary">
 	<?php dynamic_sidebar( 'homepage-cliffcare-sidebar' ); ?>
   <div class="home__cliffcare-sidebar__about">
-    <p><a href="">About CliffCare</a></p>
+    <p><a href="/cliffcare">About CliffCare</a></p>
   </div>
 </div><!-- #secondary -->
 
