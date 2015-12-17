b75c91c5d34195c1107fb4dbfecac244c9a8e1e0
Tim Lucas <t@toolmantim.com>
2015-12-17 23:03:47 +1100

    Article summary styles

--- theme/article-summary.php
+++ theme/article-summary.php
@@ -4,15 +4,15 @@
  */
 ?>
 
-<article id="article-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a class="article-summary" href="<?php echo esc_url(get_permalink()) ?>">
-    <header>
-      <h1 class="article-summary__title"><?php the_title(); ?></h1>
-      <h2 class="article-summary__date"><?php vcc_posted_on(); ?></h2>
-    </header>
+<article id="article-<?php the_ID(); ?>" <?php post_class(); ?> class="article-summary">
+  <header>
+    <h1 class="article-summary__title">
+      <a class="article-summary__title__link" href="<?php echo esc_url(get_permalink()) ?>"><?php the_title(); ?></a>
+    </h1>
+    <h2 class="article-summary__date"><?php vcc_posted_on(); ?></h2>
+  </header>
 
-    <div class="article-summary__excerpt">
-      <?php the_excerpt(); ?>
-    </div>
-  </a>
+  <div class="article-summary__excerpt">
+    <?php the_excerpt(); ?>
+  </div>
 </article>
